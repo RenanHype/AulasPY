1b7305078f347de726b4896395513f3b6f6824db
Renan Gabriel Pereira Baptista <rbaptista@codin.gov>
2025-03-06 15:55:50 -0300

    meu commit

---
+++ python_aulas/aula29.py
@@ -0,0 +1,18 @@
+'''
+Operadores de atribuição
+
+= (igual)
++= (usa o valor antigo para fazer uma soma e serve para concatenar str)
+-= (usa o valor antigo para fazer uma subtração)
+*= (usa o valor antigo para fazer uma multiplicação)
+/= (usa o valor antigo para fazer uma div com float)
+//= (usa o valor antigo para fazer uma div com int)
+**= (usa o valor antigo para fazer uma exponenciação)
+%= (usa o valor antigo para fazer um modulo)
+'''
+
+contador = 0 
+
+###
+
+contador += 1
